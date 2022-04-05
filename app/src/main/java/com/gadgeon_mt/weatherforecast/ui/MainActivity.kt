f7aa4c12850119f10d46820533ea357d05a1a885
Amal Jofy <v-ajofy@expediagroup.com>
2022-04-05 10:01:48 +0530

    Initial commit

--- app/src/main/java/com/gadgeon_mt/weatherforecast/ui/MainActivity.kt
+++ app/src/main/java/com/gadgeon_mt/weatherforecast/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gadgeon_mt.weatherforecast
+package com.gadgeon_mt.weatherforecast.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gadgeon_mt.weatherforecast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
